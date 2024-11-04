c63b9d7b42f1145cb9218a2ec2a85b98f3b4c14f
Yu-huan Kuo <touch@rnmeow.com>
2024-11-04 18:27:04 +0800

    perf: use map instead of `i.find()`

--- src/errors/http_error.ts
+++ src/errors/http_error.ts
@@ -1,49 +1,64 @@
 import { HTTPException } from 'hono/http-exception'
 import type { StatusCode } from 'hono/utils/http-status'
 
-const httpStatusList = [
-  {
-    code: 400,
-    title: 'Bad Request',
-    refSection: 'section-15.5.1',
-  },
-  {
-    code: 404,
-    title: 'Not Found',
-    refSection: 'section-15.5.5',
-  },
-  {
-    code: 418,
-    title: '(Unused)',
-    refSection: 'section-15.5.19',
-  },
-  {
-    code: 429,
-    title: 'Too Many Requests',
-    rfc6585: true,
-    refSection: 'section-4',
-  },
-  {
-    code: 500,
-    title: 'Internal Server Error',
-    refSection: 'section-15.6.1',
-  },
-  {
-    code: 501,
-    title: 'Not Implemented',
-    refSection: 'section-15.6.2',
-  },
-]
+const httpStatusMap = new Map<
+  number,
+  { title: string; refSection: string; rfc6585?: boolean }
+>([
+  [
+    400,
+    {
+      title: 'Bad Request',
+      refSection: 'section-15.5.1',
+    },
+  ],
+  [
+    404,
+    {
+      title: 'Not Found',
+      refSection: 'section-15.5.5',
+    },
+  ],
+  [
+    418,
+    {
+      title: '(Unused)',
+      refSection: 'section-15.5.19',
+    },
+  ],
+  [
+    429,
+    {
+      title: 'Too Many Requests',
+      rfc6585: true,
+      refSection: 'section-4',
+    },
+  ],
+  [
+    500,
+    {
+      title: 'Internal Server Error',
+      refSection: 'section-15.6.1',
+    },
+  ],
+  [
+    501,
+    {
+      title: 'Not Implemented',
+      refSection: 'section-15.6.2',
+    },
+  ],
+])
 
 export function createRfcHttpError(
   code: StatusCode,
   detail: string,
 ): HTTPException {
-  const match = httpStatusList.find((i) => code === i.code)
+  const match = httpStatusMap.get(code)
 
-  const title = match?.title ?? 'UNKNOWN ERROR'
-  const rfc6585 = match?.rfc6585 ?? false
-  const refSection = match?.refSection ?? ''
+  const title = match?.title ?? 'UNKNOWN ERROR',
+    rfc6585 = match?.rfc6585 ?? false,
+    refSection = match?.refSection ?? ''
 
   return new HTTPException(code, {
     res: new Response(
