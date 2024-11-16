59c78985164d69017090098cae123f6884147207
Yu-huan Kuo <touch@rnmeow.com>
2024-11-16 20:15:38 +0800

    feat: cache for shortened urls

--- src/routes/[slug].ts
+++ src/routes/[slug].ts
@@ -1,3 +1,4 @@
+import { cache } from 'hono/cache'
 import { createFactory } from 'hono/factory'
 import { logger } from 'hono/logger'
 
@@ -7,32 +8,40 @@ const factory = createFactory<{
   Bindings: { VERIFICARTION_TOKEN: string; DB: D1Database }
 }>()
 
-export const handlers = factory.createHandlers(logger(), async (ctxt) => {
-  const db = ctxt.env.DB
+export const handlers = factory.createHandlers(
+  logger(),
+  cache({
+    cacheName: 'shortened_url',
+    cacheControl: 'max-age=172800', // 48 hours
+    keyGenerator: (ctxt) => ctxt.req.path,
+  }),
+  async (ctxt) => {
+    const db = ctxt.env.DB
 
-  // should've been checked in route with regexp
-  const { slug } = ctxt.req.param()
+    // should've been checked in route with regexp
+    const { slug } = ctxt.req.param()
 
-  const { success, results } = await db
-    .prepare(`SELECT destination FROM URLs WHERE slug = ?;`)
-    .bind(slug)
-    .all()
+    const { success, results } = await db
+      .prepare(`SELECT destination FROM URLs WHERE slug = ?;`)
+      .bind(slug)
+      .all()
 
-  if (!success || results.length > 1) {
-    throw createRfcHttpError(500, 'Error reading data from the database')
-  }
-  if (results.length === 0) {
-    throw createRfcHttpError(
-      404,
-      'The requested page may have been removed or renamed',
-    )
-  }
+    if (!success || results.length > 1) {
+      throw createRfcHttpError(500, 'Error reading data from the database')
+    }
+    if (results.length === 0) {
+      throw createRfcHttpError(
+        404,
+        'The requested page may have been removed or renamed',
+      )
+    }
 
-  return ctxt.body(null, {
-    status: 301,
-    headers: new Headers({
-      'Location': results[0].destination as string,
-      'Cache-Control': 'max-age=3600',
-    }),
-  })
-})
+    return ctxt.body(null, {
+      status: 301,
+      headers: new Headers({
+        'Location': results[0].destination as string,
+        'Cache-Control': 'max-age=3600',
+      }),
+    })
+  },
+)
